3ccbeafc933aedc92617a55ae44157fc137be220
jesusjzp <jesusjzp@gmail.com>
2014-08-01 17:26:59 -0700

    Fixed more tests

--- test/wikitest.js
+++ test/wikitest.js
@@ -1,7 +1,7 @@
 var assert = require("assert");
 var parser = require('../lib/infobox.js');
 
-describe('Wiki Parser', function(done) {
+describe('Wiki parser successfully.', function(done) {
 
   it('should return json data', function(done) {
     parser.parseWiki('france', function(err, result) {
@@ -19,20 +19,20 @@ describe('Wiki Parser', function(done) {
 
 });
 
-describe('Wiki parser not found', function(done) {
+describe('Wiki parser not found.', function(done) {
 
-  it('should return Not found', function(done) {
+  it('should return Page Index Not Found', function(done) {
     parser.parseWiki('nomatterwhathere', function(err, result) {
       assert.equal(true, result === 'Page Index Not Found');
       done(err);
     });
   });
 
-  it('should return Not found', function(done) {
+  it('should return Query Not Found', function(done) {
     parser.parseWiki('', function(err, result) {
       assert.equal(true, result === 'Query Not Found');
       done(err);
     });
   });
-  
+
 });
